17fb5d1648006a1dac907d4775531a6d827843d8
qinnei <101728303+qinnei@users.noreply.github.com>
2023-06-02 23:45:41 +0700

    payment and order

---
+++ order.php
@@ -0,0 +1,27 @@
+<?php
+include('include/connect.php');
+include('function/common_functions.php');
+
+if (isset($_GET['user_id'])){
+    $user_id=$GET['user_id'];
+
+}
+
+//geting total items anf total price of all items
+$get_ip_address=getIPAddress();
+$total_price=0;
+$cart_query_price="Select * from `cart_details` where ip_address='$get_ip_address'";
+$result_cart_price=mysqli_query($con,$cart_query_price);
+$count_product=mysqli_num_rows($result_cart_price);
+while($row_price=mysqli_fetch_array($result_cart_price)){
+    $product_id=$row_price['product_id'];
+    $select_product="Select * from `products` product_id=$product_id";
+    $run_price=mysqli_query($con,$select_product);
+    while ($row_product_price=mysqli_fetch_array($run_price)){
+        $product_price=array($row_product_price['product_price']);
+        $product_values=array_sum($product_price);  
+        $total_price+=$product_values;      
+    }
+}
+?>
+
--- payment.php
+++ payment.php
@@ -1,3 +1,8 @@
+<?php
+global $con;
+include('include/connect.php');
+include('function/common_functions.php')
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,9 +10,67 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Payment page</title>
+    <meta name="description" content="Responsive Bootstrap4 Shop Template, Created
+			by Imran Hossain from https://imransdesign.com/">
+
+	<!-- title -->
+	<title>Kiwi - Flower Shop</title>
+
+	<!-- favicon -->
+	<link rel="shortcut icon" type="image/png" href="assets/img/favicon.png">
+	<!-- google font -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700"
+		  rel="stylesheet">
+	<link
+		href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap"
+		rel="stylesheet">
+	<!-- fontawesome -->
+	<link rel="stylesheet" href="assets/css/all.min.css">
+	<!-- bootstrap -->
+	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+	<!-- owl carousel -->
+	<link rel="stylesheet" href="assets/css/owl.carousel.css">
+	<!-- magnific popup -->
+	<link rel="stylesheet" href="assets/css/magnific-popup.css">
+	<!-- animate css -->
+	<link rel="stylesheet" href="assets/css/animate.css">
+	<!-- mean menu css -->
+	<link rel="stylesheet" href="assets/css/meanmenu.min.css">
+	<!-- main style -->
+	<link rel="stylesheet" href="assets/css/main.css">
+	<!-- responsive -->
+	<link rel="stylesheet" href="assets/css/responsive.css">
 </head>
+<style>
+    .payment_img{
+        width: 80%;
+        margin: auto;
+        display: block;
+    }
+</style>
 <body>
-    <h1>Payment</h1>
+    <!--php code to access user id-->
+    <?php
+        // $user_ip=getIPAddress();
+        $user_ip="::1";
+        $get_user="Select * from `user_table` where user_ip='$user_ip'";
+        $result=mysqli_query($con,$get_user);
+        $run_query=mysqli_fetch_array($result);
+        $user_id=$run_query['user_id'];
+    ?>
+    <div class="container">
+        <h2 class="text-center text-info">Payment option</h2>
+        <div class="row d-flex justify-content-center align-items-center my-5">
+            <div class="col-md-6">
+                <a href="https://www.paypal.com" target="_blank">
+                    <img src="../assets/img/paypal.png" alt="paypal" class="payment_img">
+                </a>
+            </div>
+            <div class="col-md-6">
+                <a href="order.php?user_id=<?php echo $user_id ?>"><h2 class="text-center">Cash On Delivery<h2></a>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
