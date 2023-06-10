ece07ce9e53a0b7c8e5689537b4f669fa0b9e26c
Yến <51265048+Sherlockian1212@users.noreply.github.com>
2023-06-10 13:06:12 +0700

    Update about.php

--- about.php
+++ about.php
@@ -59,18 +59,13 @@
 						<!-- menu start -->
 						<nav class="main-menu">
 							<ul>
-								<li class="current-list-item"><a href="index.php">Trang chủ</a>
+								<li><a href="index.php">Home</a>
 								</li>
-								<li><a href="shop.php">Sản phẩm</a>
-									<ul class="sub-menu">
-										<li><a href="shop.php">Tất cả sản phẩm</a></li>
-										<li><a href="cart.php">Giỏ hàng</a></li>
-										<li><a href="user_area/checkout.php">Đặt hàng</a></li>
-									</ul>
+								<li><a href="shop.php">Product</a>
 								</li>
-								<li><a href="about.html">Về chúng tôi</a>
+								<li class="current-list-item"><a href="about.html">About us</a>
 								</li>
-								<li><a href="contact.php">Liên hệ</a></li>
+								<li><a href="contact.php">Contact</a></li>
 								<li>
 									<div class="header-icons">
 										<a href="./user_area/profile.php" class="customer-account">
@@ -96,23 +91,23 @@
 	
 
 	<!-- search area -->
-	<div class="search-area">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12">
-					<span class="close-btn"><i class="fas fa-window-close"></i></span>
-					<div class="search-bar">
-						<div class="search-bar-tablecell">
-							<h3>Bạn muốn tìm gì?</h3>
-							<input type="text" placeholder="Từ khóa">
-							<button type="submit">Tìm kiếm<i class="fas fa-search"></i></button>
-						</div>
+<div class="search-area">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<span class="close-btn"><i class="fas fa-window-close"></i></span>
+				<div class="search-bar">
+					<div class="search-bar-tablecell">
+						<h3>What do you want to search?</h3>
+						<input type="text" placeholder="Key">
+						<button type="submit">Search<i class="fas fa-search"></i></button>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	<!-- end search area -->
+</div>
+<!-- end search area -->
 
 	
 	<!-- breadcrumb-section -->
@@ -121,8 +116,8 @@
 			<div class="row">
 				<div class="col-lg-8 offset-lg-2 text-center">
 					<div class="breadcrumb-text">
-						<p>Biệt đội Kiwi</p>
-						<h1>Về chúng tôi</h1>
+						<p>Kiwi Squad</p>
+						<h1>About us</h1>
 					</div>
 				</div>
 			</div>
@@ -136,7 +131,7 @@
 			<div class="row">
 				<div class="col-lg-7">
 					<div class="featured-text">
-						<h2 class="pb-3">Đến với <span class="orange-text">Kiwi - Flower Shop</span></h2>
+						<h2 class="pb-3">Come to <span class="orange-text">Kiwi - Flower Shop</span></h2>
 						<div class="row">
 							<div class="col-lg-6 col-md-6 mb-4 mb-md-5">
 								<div class="list-box d-flex">
@@ -144,8 +139,8 @@
 										<i class="fas fa-shipping-fast"></i>
 									</div>
 									<div class="content">
-										<h3>Giao hàng tận nơi</h3>
-										<p>Ngồi một chỗ và đợi người đẹp mang hoa đẹp tới!</p>
+										<h3>Delivery</h3>
+										<p>Sit back and wait for the beauty to bring beautiful flowers!</p>
 									</div>
 								</div>
 							</div>
@@ -155,8 +150,8 @@
 										<i class="fas fa-money-bill-alt"></i>
 									</div>
 									<div class="content">
-										<h3>Giá tốt hàng đẹp</h3>
-										<p>Giá cả hợp lí, giá tốt nhất trong thị trường</p>
+										<h3>Good price good product</h3>
+										<p>Reasonable price, best price in the market</p>
 									</div>
 								</div>
 							</div>
@@ -166,8 +161,8 @@
 										<i class="fas fa-briefcase"></i>
 									</div>
 									<div class="content">
-										<h3>Đáp ứng yêu cầu của bạn</h3>
-										<p>Nhu cầu của bạn được chúng tôi hết lòng đáp ứng</p>
+										<h3>Meet your requirements</h3>
+										<p>We wholeheartedly meet your needs</p>
 									</div>
 								</div>
 							</div>
@@ -177,8 +172,8 @@
 										<i class="fas fa-sync-alt"></i>
 									</div>
 									<div class="content">
-										<h3>Hoàn tiền nhanh chóng</h3>
-										<p>Cam kết hoàn tiền nếu không đúng yêu cầu của bạn</p>
+										<h3>Quick refund</h3>
+										<p>Guaranteed refund if not according to your request</p>
 									</div>
 								</div>
 							</div>
@@ -193,9 +188,9 @@
 	<!-- shop banner -->
 	<section class="shop-banner">
     	<div class="container">
-        	<h3>Tháng Tốt nghiệp sắp đến <br> với <span class="orange-text">khuyến mãi lớn...</span></h3>
+        	<h3>Graduation month is coming <br> with <span class="orange-text">khuyến mãi lớn...</span></h3>
             <div class="sale-percent"><span>Sale! <br> Upto</span>50% <span>off</span></div>
-            <a href="shop.php" class="cart-btn btn-lg">Mua ngay</a>
+            <a href="shop.php" class="cart-btn btn-lg">Buy now</a>
         </div>
     </section>
 	<!-- end shop banner -->
@@ -206,8 +201,8 @@
 			<div class="row">
 				<div class="col-lg-8 offset-lg-2 text-center">
 					<div class="section-title">
-						<h3>Biệt đội <span class="orange-text">Kiwi</span></h3>
-						<p>Thà chết 1 đóng còn hơn sống 1 mình</p>
+						<h3>Kiwi <span class="orange-text">Squad</span></h3>
+						<p>Thà chết 1 đống còn hơn sống 1 mình</p>
 					</div>
 				</div>
 			</div>
@@ -226,7 +221,7 @@
 				<div class="col-lg-4 col-md-6">
 					<div class="single-team-item">
 						<div class="team-bg team-bg-2"></div>
-						<h4>Thái Thị Kim Yến<span>Fontend Developer</span></h4>
+						<h4>Thái Thị Kim Yến<span>Frontend Developer</span></h4>
 						<ul class="social-link-team">
 							<li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
 							<li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
@@ -237,7 +232,7 @@
 				<div class="col-lg-4 col-md-6">
 					<div class="single-team-item">
 						<div class="team-bg team-bg-3"></div>
-						<h4>Trần Tú Quyên<span>UX-UI Designer</span></h4>
+						<h4>Trần Tú Quyên<span>UX-UI Designer - Backend Developer</span></h4>
 						<ul class="social-link-team">
 							<li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
 							<li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
@@ -248,18 +243,7 @@
 				<div class="col-lg-4 col-md-6">
 					<div class="single-team-item">
 						<div class="team-bg team-bg-4"></div>
-						<h4>Hồ Sĩ Thiện<span>Fontend Developer</span></h4>
-						<ul class="social-link-team">
-							<li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-							<li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
-							<li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6 offset-md-3 offset-lg-0">
-					<div class="single-team-item">
-						<div class="team-bg team-bg-5"></div>
-						<h4>Võ Hoài Bảo<span>Backend Developer</span></h4>
+						<h4>Hồ Sĩ Thiện<span>Frontend Developer - Backend Developer</span></h4>
 						<ul class="social-link-team">
 							<li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
 							<li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
@@ -272,65 +256,65 @@
 	</div>
 	<!-- end team section -->
 
-	<!-- footer -->
-	<div class="footer-area">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-4 col-md-6">
-					<div class="footer-box about-widget">
-						<h2 class="widget-title">Kiwi</h2>
-						<p>Kiwi luôn dành cho bạn những bông hoa đẹp nhất</p>
-					</div>
+<!-- footer -->
+<div class="footer-area">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4 col-md-6">
+				<div class="footer-box about-widget">
+					<h2 class="widget-title">Kiwi</h2>
+					<p>Kiwi always gives you the most beautiful flowers</p>
 				</div>
-				<div class="col-lg-4 col-md-6">
-					<div class="footer-box get-in-touch">
-						<h2 class="widget-title">Liên hệ</h2>
-						<ul>
-							<li>Địa chỉ: 280 An Dương Vương, P.4, Q.5, TPHCM</li>
-							<li>Email: kiwishop@gmail.com</li>
-							<li>Hotline: 1900100C0</li>
-						</ul>
-					</div>
+			</div>
+			<div class="col-lg-4 col-md-6">
+				<div class="footer-box get-in-touch">
+					<h2 class="widget-title">Contact</h2>
+					<ul>
+						<li>Address: 280 An Duong Vuong, Ward 4, District 5, HCMC</li>
+						<li>Email: kiwishop@gmail.com</li>
+						<li>Hotline: 1900100C0</li>
+					</ul>
 				</div>
-				<div class="col-lg-4 col-md-6">
-					<div class="footer-box pages">
-						<h2 class="widget-title">Hỗ trợ khách hàng</h2>
-						<ul>
-							<li>Câu hỏi thường gặp</li>
-							<li>Hướng dẫn đặt hàng</li>
-							<li>Hướng dẫn thanh toán</li>
-						</ul>
-					</div>
+			</div>
+			<div class="col-lg-4 col-md-6">
+				<div class="footer-box pages">
+					<h2 class="widget-title">Customer support</h2>
+					<ul>
+						<li>Frequently asked questions</li>
+						<li>Ordering guide</li>
+						<li>Payment Guide</li>
+					</ul>
 				</div>
 			</div>
 		</div>
 	</div>
-	<!-- end footer -->
+</div>
+<!-- end footer -->
 
-	<!-- copyright -->
-	<div class="copyright">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-6 col-md-12">
-					<p>Copyrights &copy; 2023 - <a href="#">Kiwi Group</a>, All Rights
-						Reserved.<br>
-					</p>
-				</div>
-				<div class="col-lg-6 text-right col-md-12">
-					<div class="social-icons">
-						<ul>
-							<li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-							<li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
-							<li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
-							<li><a href="#" target="_blank"><i class="fab fa-linkedin"></i></a></li>
-							<li><a href="#" target="_blank"><i class="fab fa-dribbble"></i></a></li>
-						</ul>
-					</div>
+<!-- copyright -->
+<div class="copyright">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-md-12">
+				<p>Copyrights &copy; 2023 - <a href="#">Kiwi Group</a>, All Rights
+					Reserved.<br>
+				</p>
+			</div>
+			<div class="col-lg-6 text-right col-md-12">
+				<div class="social-icons">
+					<ul>
+						<li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+						<li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
+						<li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
+						<li><a href="#" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+						<li><a href="#" target="_blank"><i class="fab fa-dribbble"></i></a></li>
+					</ul>
 				</div>
 			</div>
 		</div>
 	</div>
-	<!-- end copyright -->
+</div>
+<!-- end copyright -->
 	
 	<!-- jquery -->
 	<script src="assets/js/jquery-1.11.3.min.js"></script>
