5c743bdf4dcfbe0215f7bf8b80464f5af33e3fc8
Yến <51265048+Sherlockian1212@users.noreply.github.com>
2023-06-10 16:14:40 +0700

    Update search_product.php

--- search_product.php
+++ search_product.php
@@ -5,6 +5,7 @@ include('function/common_functions.php')
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -36,224 +37,221 @@ include('function/common_functions.php')
     <!-- responsive -->
     <link rel="stylesheet" href="assets/css/responsive.css">
 </head>
+
 <body>
 
-<!--PreLoader-->
-<div class="loader">
-    <div class="loader-inner">
-        <div class="circle"></div>
+    <!--PreLoader-->
+    <div class="loader">
+        <div class="loader-inner">
+            <div class="circle"></div>
+        </div>
     </div>
-</div>
-<!--PreLoader Ends-->
+    <!--PreLoader Ends-->
 
-<!-- header -->
-<div class="top-header-area" id="sticker">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 col-sm-12 text-center">
-                <div class="main-menu-wrap">
-                    <!-- logo -->
-                    <div class="site-logo">
-                        <a href="index.php">
-                            <img src="assets/img/logo.png" alt="">
-                        </a>
-                    </div>
-                    <!-- logo -->
+    <!-- header -->
+    <div class="top-header-area" id="sticker">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-sm-12 text-center">
+                    <div class="main-menu-wrap">
+                        <!-- logo -->
+                        <div class="site-logo">
+                            <a href="index.php">
+                                <img src="assets/img/logo.png" alt="">
+                            </a>
+                        </div>
+                        <!-- logo -->
 
-                    <!-- menu start -->
-                    <nav class="main-menu">
-                        <ul>
-                            <li class="current-list-item"><a href="index.php">Trang chủ</a>
-                            </li>
-                            <li><a href="shop.php">Sản phẩm</a>
-                                <ul class="sub-menu">
-                                    <li><a href="shop.php">Tất cả sản phẩm</a></li>
-                                    <li><a href="cart.php">Giỏ hàng</a></li>
-                                    <li><a href="user_area/checkout.php">Đặt hàng</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="about.php">Về chúng tôi</a>
-                            </li>
-                            <li><a href="contact.php">Liên hệ</a></li>
-                            <li>
-                                <div class="header-icons">
-                                    <a href="user.html" class="customer-account">
-                                        <i class="fas fa-user-alt"></i>
-                                    </a>
-                                    <a class="shopping-cart" href="cart.php"><i class="fas
+                        <!-- menu start -->
+                        <nav class="main-menu">
+                            <ul>
+                                <li><a href="index.php">Home</a>
+                                </li>
+                                <li class="current-list-item"><a href="shop.php">Product</a>
+                                </li>
+                                <li><a href="about.php">About us</a>
+                                </li>
+                                <li><a href="contact.php">Contact</a></li>
+                                <li>
+                                    <div class="header-icons">
+                                        <a href="user.html" class="customer-account">
+                                            <i class="fas fa-user-alt"></i>
+                                        </a>
+                                        <a class="shopping-cart" href="cart.php"><i class="fas
 												fa-shopping-cart"></i></a>
-                                    <a class="mobile-hide search-bar-icon" href="#"><i class="fas
+                                        <a class="mobile-hide search-bar-icon" href="#"><i class="fas
 												fa-search"></i></a>
-                                </div>
-                            </li>
-                        </ul>
-                    </nav>
-                    <a class="mobile-show search-bar-icon" href="#"><i class="fas fa-search"></i></a>
-                    <div class="mobile-menu"></div>
-                    <!-- menu end -->
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- end header -->
-
-<?php
-    cart();
-?>
-<!-- search area -->
-<div class="search-area">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <span class="close-btn"><i class="fas fa-window-close"></i></span>
-                <form class="search-bar" action="" method="get">
-                    <div class="search-bar-tablecell">
-                        <h3>Search For:</h3>
-                        <input type="search" placeholder="Keywords" name="search_data">
-                        <button name="search_data_product" value="Search" type="submit">Search<i class="fas fa-search"></i></button>
+                                    </div>
+                                </li>
+                            </ul>
+                        </nav>
+                        <a class="mobile-show search-bar-icon" href="#"><i class="fas fa-search"></i></a>
+                        <div class="mobile-menu"></div>
+                        <!-- menu end -->
                     </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- end search arewa -->
-
-<!-- breadcrumb-section -->
-<div class="breadcrumb-section breadcrumb-bg">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 offset-lg-2 text-center">
-                <div class="breadcrumb-text">
-                    <p>Luôn mang đến cho bạn những bông hoa đẹp nhất</p>
-                    <h1>Sản phẩm của chúng tôi</h1>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<!-- end breadcrumb section -->
+    <!-- end header -->
 
-<!-- products -->
-<div class="product-section mt-100 mb-50">
-    <div class="container">
-
-        <div class="row">
-            <div class="col-md-12">
-                <div class="product-filters">
-                    <ul>
-                        <?php
-                        getCategories();
-                        ?>
-                    </ul>
+    <?php
+    cart();
+    ?>
+    <!-- search area -->
+    <div class="search-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <span class="close-btn"><i class="fas fa-window-close"></i></span>
+                    <div class="search-bar">
+                        <div class="search-bar-tablecell">
+                            <h3>What do you want to search?</h3>
+                            <input type="text" placeholder="Key">
+                            <button type="submit">Search<i class="fas fa-search"></i></button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="row product-lists">
-            <?php
-                if(!isset($_GET['search_data_product']))
+    </div>
+    <!-- end search area -->
+
+    <!-- breadcrumb-section -->
+    <div class="breadcrumb-section breadcrumb-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2 text-center">
+                    <div class="breadcrumb-text">
+                        <p>Always bring you the most beautiful flowers</p>
+                        <h1>Our products</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end breadcrumb section -->
+
+    <!-- products -->
+    <div class="product-section mt-100 mb-50">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="product-filters">
+                        <ul>
+                            <?php
+                            getCategories();
+                            ?>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row product-lists">
+                <?php
+                if (!isset($_GET['search_data_product']))
                     getProducts();
                 else
                     searchProduct();
-            ?>
-            <!--			<div class="row">-->
-            <!--				<div class="col-lg-12 text-center p-20">-->
-            <!--					<div class="pagination-wrap m-0">-->
-            <!--						<ul>-->
-            <!--							<li><a href="#">Prev</a></li>-->
-            <!--							<li><a href="#">1</a></li>-->
-            <!--							<li><a class="active" href="#">2</a></li>-->
-            <!--							<li><a href="#">3</a></li>-->
-            <!--							<li><a href="#">Next</a></li>-->
-            <!--						</ul>-->
-            <!--					</div>-->
-            <!--				</div>-->
-            <!--			</div>-->
+                ?>
+                <!--			<div class="row">-->
+                <!--				<div class="col-lg-12 text-center p-20">-->
+                <!--					<div class="pagination-wrap m-0">-->
+                <!--						<ul>-->
+                <!--							<li><a href="#">Prev</a></li>-->
+                <!--							<li><a href="#">1</a></li>-->
+                <!--							<li><a class="active" href="#">2</a></li>-->
+                <!--							<li><a href="#">3</a></li>-->
+                <!--							<li><a href="#">Next</a></li>-->
+                <!--						</ul>-->
+                <!--					</div>-->
+                <!--				</div>-->
+                <!--			</div>-->
+            </div>
         </div>
-    </div>
-    <!-- end products -->
+        <!-- end products -->
 
-    <!-- footer -->
-    <div class="footer-area mt-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-md-6">
-                    <div class="footer-box about-widget">
-                        <h2 class="widget-title">Kiwi</h2>
-                        <p>Kiwi luôn dành cho bạn những bông hoa đẹp nhất</p>
+        <!-- footer -->
+        <div class="footer-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="footer-box about-widget">
+                            <h2 class="widget-title">Kiwi</h2>
+                            <p>Kiwi always gives you the most beautiful flowers</p>
+                        </div>
                     </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="footer-box get-in-touch">
-                        <h2 class="widget-title">Liên hệ</h2>
-                        <ul>
-                            <li>Địa chỉ: 280 An Dương Vương, P.4, Q.5, TPHCM</li>
-                            <li>Email: kiwishop@gmail.com</li>
-                            <li>Hotline: 1900100C0</li>
-                        </ul>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="footer-box get-in-touch">
+                            <h2 class="widget-title">Contact</h2>
+                            <ul>
+                                <li>Address: 280 An Duong Vuong, Ward 4, District 5, HCMC</li>
+                                <li>Email: kiwishop@gmail.com</li>
+                                <li>Hotline: 1900100C0</li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="footer-box pages">
-                        <h2 class="widget-title">Hỗ trợ khách hàng</h2>
-                        <ul>
-                            <li>Câu hỏi thường gặp</li>
-                            <li>Hướng dẫn đặt hàng</li>
-                            <li>Hướng dẫn thanh toán</li>
-                        </ul>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="footer-box pages">
+                            <h2 class="widget-title">Customer support</h2>
+                            <ul>
+                                <li>Frequently asked questions</li>
+                                <li>Ordering guide</li>
+                                <li>Payment Guide</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <!-- end footer -->
+        <!-- end footer -->
 
-    <!-- copyright -->
-    <div class="copyright">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 col-md-12">
-                    <p>Copyrights &copy; 2023 - <a href="#">Kiwi Group</a>, All Rights
-                        Reserved.<br>
-                    </p>
-                </div>
-                <div class="col-lg-6 text-right col-md-12">
-                    <div class="social-icons">
-                        <ul>
-                            <li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
-                            <li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
-                            <li><a href="#" target="_blank"><i class="fab fa-linkedin"></i></a></li>
-                            <li><a href="#" target="_blank"><i class="fab fa-dribbble"></i></a></li>
-                        </ul>
+        <!-- copyright -->
+        <div class="copyright">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-12">
+                        <p>Copyrights &copy; 2023 - <a href="#">Kiwi Group</a>, All Rights
+                            Reserved.<br>
+                        </p>
+                    </div>
+                    <div class="col-lg-6 text-right col-md-12">
+                        <div class="social-icons">
+                            <ul>
+                                <li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                                <li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                                <li><a href="#" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+                                <li><a href="#" target="_blank"><i class="fab fa-dribbble"></i></a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <!-- end copyright -->
+        <!-- end copyright -->
 
-    <!-- jquery -->
-    <script src="assets/js/jquery-1.11.3.min.js"></script>
-    <!-- bootstrap -->
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-    <!-- count down -->
-    <script src="assets/js/jquery.countdown.js"></script>
-    <!-- isotope -->
-    <script src="assets/js/jquery.isotope-3.0.6.min.js"></script>
-    <!-- waypoints -->
-    <script src="assets/js/waypoints.js"></script>
-    <!-- owl carousel -->
-    <script src="assets/js/owl.carousel.min.js"></script>
-    <!-- magnific popup -->
-    <script src="assets/js/jquery.magnific-popup.min.js"></script>
-    <!-- mean menu -->
-    <script src="assets/js/jquery.meanmenu.min.js"></script>
-    <!-- sticker js -->
-    <script src="assets/js/sticker.js"></script>
-    <!-- main js -->
-    <script src="assets/js/main.js"></script>
+        <!-- jquery -->
+        <script src="assets/js/jquery-1.11.3.min.js"></script>
+        <!-- bootstrap -->
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+        <!-- count down -->
+        <script src="assets/js/jquery.countdown.js"></script>
+        <!-- isotope -->
+        <script src="assets/js/jquery.isotope-3.0.6.min.js"></script>
+        <!-- waypoints -->
+        <script src="assets/js/waypoints.js"></script>
+        <!-- owl carousel -->
+        <script src="assets/js/owl.carousel.min.js"></script>
+        <!-- magnific popup -->
+        <script src="assets/js/jquery.magnific-popup.min.js"></script>
+        <!-- mean menu -->
+        <script src="assets/js/jquery.meanmenu.min.js"></script>
+        <!-- sticker js -->
+        <script src="assets/js/sticker.js"></script>
+        <!-- main js -->
+        <script src="assets/js/main.js"></script>
 
 </body>
+
 </html>
